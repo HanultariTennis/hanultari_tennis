296758db9c501110a55696046bfa23dede42bd5a
Hanultari Tennis <hanultaritennis@gmail.com>
2025-01-05 10:22:21 +0900

    Change port number

--- app.js
+++ app.js
@@ -229,7 +229,7 @@ http.createServer(httpApp).listen(80, () => {
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
